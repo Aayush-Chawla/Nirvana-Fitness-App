794dace35d3fc3c5f9d5bbb0aeac04044de8e3c1
Aayush Chawla <aayushchawla5554@gmail.com>
2025-04-09 23:03:18 +0530

    "Full Log Diet" working and removed duplicate "Quick Food Log"

--- app/src/main/java/com/example/nirvana/fragments/diet/DietFragment.java
+++ app/src/main/java/com/example/nirvana/fragments/diet/DietFragment.java
@@ -32,7 +32,6 @@ public class DietFragment extends Fragment {
 
     private PieChart macrosChart;
     private MaterialButton btnLogDiet;
-    private MaterialButton btnQuickLog;
     private TextView tvCalories, tvRemaining, tvGoal;
 
     private FirebaseAuth mAuth;
@@ -55,7 +54,6 @@ public class DietFragment extends Fragment {
     private void initializeViews(View view) {
         macrosChart = view.findViewById(R.id.macrosChart);
         btnLogDiet = view.findViewById(R.id.btnLogDiet);
-        btnQuickLog = view.findViewById(R.id.btnQuickLog);
         tvCalories = view.findViewById(R.id.tvCalories);
         tvRemaining = view.findViewById(R.id.tvRemaining);
         tvGoal = view.findViewById(R.id.tvGoal);
@@ -70,9 +68,6 @@ public class DietFragment extends Fragment {
     private void setupButtonListeners() {
         btnLogDiet.setOnClickListener(v ->
                 Navigation.findNavController(v).navigate(R.id.action_dietFragment_to_logDietFragment));
-        
-        btnQuickLog.setOnClickListener(v ->
-                Navigation.findNavController(v).navigate(R.id.action_dietFragment_to_logFoodFragment));
     }
 
     private void loadUserDietData() {
